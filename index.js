cdb7d1b697d9dd4976913f7cd0b18dc3cd82d11d
Nicky Kimaina <nkimaina@ampath.or.ke>
2018-04-09 15:43:02 +0300

    Added filed formatter for navigating to a patient list

---
+++ index.js
@@ -0,0 +1,33 @@
+import { resolve } from 'path';
+
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+    name: 'patient-nav',
+    uiExports: {
+
+      fieldFormats: ['plugins/patient-nav/p-nav'],
+
+
+      translations: [
+        resolve(__dirname, './translations/es.json')
+      ],
+
+
+      hacks: [
+        'plugins/patient-nav/hack'
+      ]
+
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+
+
+  });
+}
---
+++ public/p-nav.js
@@ -0,0 +1,49 @@
+import { RegistryFieldFormatsProvider } from 'ui/registry/field_formats';
+// import { RegistryFieldFormatEditorsProvider } from 'ui/registry/field_format_editors';
+import _ from 'lodash';
+
+export function createPatientNav(FieldFormat) {
+  class PatientNav extends FieldFormat {
+
+    constructor(params) {
+      super(params);
+    }
+
+    static id = 'patient-nav';
+    static title = 'Patient Navigation';
+    static fieldType = [
+      'string'
+    ];
+
+  }
+
+  PatientNav.prototype._convert = {
+    text(val) {
+      return val;
+    },
+    html(val, field, hit) {
+      // console.log('Passed values', val, field, hit);
+      let html = '';
+      // html += `
+      //   <script type="text/javascript>
+      //   function onclickPatientNav() {
+      //     console.log('button clicked');
+      //   }
+      //   </script>
+      // `;
+      html = html + '<button type="button" class="btn btn-default" onclick="' + `
+      (function onclickPatientNav() {
+             console.log('Navigating to patient', '` + val + `');
+             window.parent.postMessage('` + val + `', '*');       
+      })();
+      ` +
+      '">View Patient</button>';
+
+      // console.log('=======>', html);
+      return html;
+    }
+  };
+  return PatientNav;
+}
+
+RegistryFieldFormatsProvider.register(createPatientNav);
